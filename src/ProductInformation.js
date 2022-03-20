d43263511e0b0bbec5d04a1249e489a61606fa2a
lkbobadi <lkbobadi@gmail.com>
2022-03-20 20:24:13 +0900

    fixed discount position

--- src/ProductInformation.js
+++ src/ProductInformation.js
@@ -1,10 +1,11 @@
 import PurchaseProduct from "./PurchaseProduct";
+import "./Purchase.css";
 
 export default function ProductInformation() {
   return (
     <main className='main-information col-md-6'>
-      Sneaker Company
-      <h2>Fall Limited Sneakers</h2>
+      <subtitle>Sneaker Company</subtitle>
+      <h1>Fall Limited Edition Sneakers</h1>
       <p>
         These low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they'll withstand everything the
@@ -12,7 +13,7 @@ export default function ProductInformation() {
       </p>
       <div className='price'>
         $125.00<span className='discount'> 50%</span>
-        <div className='original-price'>$225.00</div>
+        <div className='original-price mt-2 mb-5'>$225.00</div>
       </div>
       <div className='purchase-section'>
         <PurchaseProduct />
