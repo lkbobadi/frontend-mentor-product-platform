a24f24b1bcf140469ff1b1d95afb5dacfa418d95
lkbobadi <lkbobadi@gmail.com>
2022-03-27 00:47:53 +0900

    Toggled Opacity
    Individual Images are toggleable now

--- src/ProductGallery.js
+++ src/ProductGallery.js
@@ -7,36 +7,24 @@ import product3 from "../src/images/image-product-3.jpg";
 import product4 from "../src/images/image-product-4.jpg";
 
 export default function ProductGallery() {
-  let [mainImage, setMainImage] = useState(product1);
-
-  let [opacity, setOpacity] = useState(1);
-
-  let images = [
-    { photo: product1 },
-    { photo: product2 },
-    { photo: product3 },
-    { photo: product4 },
-  ];
-
-  function clickImage(e) {
-    setOpacity(!opacity);
-    setMainImage(e.target.src);
-    e.target.style.opacity = 0.7;
-  }
+  let images = [product1, product2, product3, product4];
+  let [selectImage, setSelectImage] = useState(images[0]);
 
   return (
     <div className='gallery col-md-6'>
-      <img src={mainImage} alt='product shoes' className='main-image' />
+      <img src={selectImage} alt='product shoes' className='main-image' />
 
       <section className='extra-images'>
         {images.map((image, index) => {
           return (
-            <div key={index}>
+            <div key={index} value={index}>
               <img
-                src={image.photo}
-                onClick={clickImage}
+                src={image}
+                onClick={() => setSelectImage(image)}
                 alt={index}
-                style={{ opacity: opacity ? index.length + 0.7 : 1 }}
+                style={{
+                  opacity: selectImage === image ? 0.7 : "",
+                }}
               />
             </div>
           );
