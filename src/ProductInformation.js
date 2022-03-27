114022f1381704782a61a95fc1af5e8b4fca61ae
lkbobadi <lkbobadi@gmail.com>
2022-03-27 17:10:41 +0900

    Main-Information css responsive

--- src/ProductInformation.js
+++ src/ProductInformation.js
@@ -3,10 +3,10 @@ import "./Purchase.css";
 
 export default function ProductInformation() {
   return (
-    <main className='main-information col-sm-6 '>
+    <main className='main-information col-sm-6 g-0 p-4'>
       <div className='subtitle'>Sneaker Company</div>
       <h1>Fall Limited Edition Sneakers</h1>
-      <p className='g-0'>
+      <p>
         These low-profile sneakers are your perfect casual wear companion.
         Featuring a durable rubber outer sole, they'll withstand everything the
         weather can offer.
