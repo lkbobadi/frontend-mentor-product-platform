ebb4f083f4265b22ed9ae0e64050c797e925020b
lkbobadi <lkbobadi@gmail.com>
2022-04-22 18:30:19 +0900

    responsive nav update
    responsive nav update

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,32 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import "./PurchaseNav.css";
 import logo from "../src/images/logo.svg";
+import { IconContext } from "react-icons";
+import { GiHamburgerMenu } from "react-icons/gi";
 import PurchaseNav from "./PurchaseNav";
 
 export default function Navigation() {
-  return (
-    <ul className='navigation col-md-6'>
-      <li>
-        <img src={logo} alt='logo' className='logo' />
-      </li>
-      <li>
-        <a href='/collections'>Collections</a>
-      </li>
-      <li>
-        <a href='/men'>Men</a>
-      </li>
-      <li>
-        <a href='/women'>Women</a>
-      </li>
-      <li>
-        <a href='/collections'>About</a>
-      </li>
-      <li>
-        <a href='/contact'>Contact</a>
-      </li>
+  const [isMenuOpen, setIsMenuOpen] = useState("false");
+  let [menu, setMenu] = useState("translateX(0%)");
 
-      <PurchaseNav />
-    </ul>
+  function openMenu() {
+    isMenuOpen = !isMenuOpen;
+    if (isMenuOpen === true) {
+      setMenu("translateX(0%)");
+    } else {
+      setMenu("translateX(-100%)");
+    }
+  }
+  return (
+    <>
+      <div className='navburger'>
+        <IconContext.Provider
+          value={{
+            color: "white",
+            size: "1.2em",
+            className: "global-class-name",
+          }}
+        >
+          <GiHamburgerMenu onClick={openMenu} />
+        </IconContext.Provider>
+      </div>
+      <ul
+        className={`navigation col-md-6 ${isMenuOpen ? "" : "translateX(0%)"}`}
+        style={{ transform: menu }}
+      >
+        <li>
+          <img src={logo} alt='logo' className='logo' />
+        </li>
+        <li>
+          <a href='/collections'>Collections</a>
+        </li>
+        <li>
+          <a href='/men'>Men</a>
+        </li>
+        <li>
+          <a href='/women'>Women</a>
+        </li>
+        <li>
+          <a href='/collections'>About</a>
+        </li>
+        <li>
+          <a href='/contact'>Contact</a>
+        </li>
+
+        <PurchaseNav />
+      </ul>
+    </>
   );
 }
--- src/ProductGallery.js
+++ src/ProductGallery.js
@@ -12,17 +12,7 @@ export default function ProductGallery() {
 
   return (
     <div className='gallery col-6 g-0'>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-      <div className='main-image'>
-=======
-=======
->>>>>>> parent of e8c881e (Mobile Responsive Nav)
-=======
->>>>>>> parent of e8c881e (Mobile Responsive Nav)
       <div className='main-image g-0'>
->>>>>>> parent of e8c881e (Mobile Responsive Nav)
         <img src={selectImage} alt='product shoes' />
       </div>
       <section className='extra-images'>
